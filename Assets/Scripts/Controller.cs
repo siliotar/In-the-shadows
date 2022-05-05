19faecd7b49581999d8e792a577ef1137fa3dec0
siliotar <nikita.yashinm@list.ru>
2022-05-05 22:10:14 +0300

    Bug Fix. Fixed a bug when only the rotation of the last object is taken into account when checking the passage of the level.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -79,6 +79,8 @@ public class Controller : MonoBehaviour
                         StartCoroutine(BackToMenu());
                     }
                 }
+                else
+                    break;
             }
         }
     }
